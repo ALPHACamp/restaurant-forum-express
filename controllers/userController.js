8f9660208008551b85510e77ecc464a2da6677e3
Ellen Lee <ellenlee@Ellende-MacBook-Pro.local>
2019-05-03 13:02:38 +0800

    add user profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcrypt-nodejs') 
 const db = require('../models')
 const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = '955ea6fa229b9c1'
 
 let userController = {
   signUpPage: (req, res) => {
@@ -43,6 +47,53 @@ let userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    }).then(user => {
+      return res.render('user/profile', { profile: user })
+    })
+  },
+
+  editUser: (req, res) => {
+    return User.findByPk(req.params.id).then(user => {
+      return res.render('user/edit', {user: user})
+    })
+  },
+  putUser: (req, res) => {
+    if (Number(req.params.id) !== Number(req.user.id)) {
+      return res.redirect(`/users/${req.params.id}`)
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: img.data.link,
+            })
+            .then((user) => {
+              res.redirect(`/users/${req.params.id}`)
+            })
+          })
+      })
+    }
+    else
+      return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name
+            })
+            .then((user) => {
+              res.redirect(`/users/${req.params.id}`)
+            })
+          })
   }
 }
 
