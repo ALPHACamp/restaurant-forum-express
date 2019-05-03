92fb90fe5937b4c5eedcab739cb4503a7ea68c37
Ellen Lee <ellenlee@Ellende-MacBook-Pro.local>
2019-05-03 15:56:50 +0800

    Finished favorite feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -27,7 +27,8 @@ let restController = {
     
     const data = result.rows.map(r => ({
        ...r.dataValues,
-       description: r.dataValues.description.substring(0, 50)
+       description: r.dataValues.description.substring(0, 50),
+       isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
      }))
      Category.findAll().then(categories => {
       return res.render('restaurants', {
@@ -44,11 +45,14 @@ let restController = {
   return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        {model: User, as: 'FavoritedUsers' },
         {model: Comment, include: [User]}
       ]
     }).then(restaurant => {
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
       return res.render('restaurant', {
-        restaurant: restaurant
+        restaurant: restaurant,
+        isFavorited: isFavorited
       })
     })
  },
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const db = require('../models')
 const User = db.User
 const Restaurant = db.Restaurant
 const Comment = db.Comment
+const Favorite = db.Favorite
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = '955ea6fa229b9c1'
 
@@ -94,7 +95,30 @@ let userController = {
               res.redirect(`/users/${req.params.id}`)
             })
           })
-  }
+  },
+
+  addFavorite: (req, res) => {
+    return Favorite.create({
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    })
+     .then((restaurant) => {
+       return res.redirect('back')
+     })
+   },
+   
+   removeFavorite: (req, res) => {
+    return Favorite.findOne({where: {
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    }})
+      .then((favorite) => {
+        favorite.destroy()
+         .then((restaurant) => {
+           return res.redirect('back')
+         })
+      })
+   }
 }
 
 module.exports = userController
